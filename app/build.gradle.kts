623d0287dbadb6a911efc8ef4e8c9c4913c160f9
harshitsharma25 <omsharma2508@gmail.com>
2025-06-30 14:45:48 +0530

    Initial project commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,86 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+
+    alias(libs.plugins.ksp)
+    id("com.google.gms.google-services")
+}
+
+android {
+    namespace = "com.nordresa.travel"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.nordresa.travel"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+
+    // firebase
+    implementation(platform("com.google.firebase:firebase-bom:33.15.0"))
+    //firebase auth
+    implementation("com.google.firebase:firebase-auth:23.1.0")
+    // firebase firestore
+    implementation("com.google.firebase:firebase-firestore:25.1.1")
+
+    // retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+
+    // ok Http
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+
+    //Glide
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
+
+    // Navigation
+    implementation ("androidx.navigation:navigation-fragment-ktx:2.7.0")
+    implementation ("androidx.navigation:navigation-ui-ktx:2.7.0")
+
+    //Room
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    ksp("androidx.room:room-compiler:2.6.1")
+
+    // KSP - Updated version
+    implementation ("com.google.devtools.ksp:symbol-processing-api:2.0.10-1.0.24")
+
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.12.0")
+}
